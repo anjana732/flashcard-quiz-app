76a91d4f7a9f4e7d889528a3e49ce808e05f4c52
anjana732 <anjanasingh1257@gmail.com>
2025-06-01 23:28:00 +0530

    Fetch data from API

--- src/components/Pages/QuizPage.jsx
+++ src/components/Pages/QuizPage.jsx
@@ -1,7 +1,20 @@
 import Popup from '../../components/UI/Popup'
+
 function QuizPage(){
+    async function fetchQuestion(amount, category, difficulty){
+        let url = `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=multiple`;
+        try
+        {
+            let response = await fetch(url);
+            if(!response.ok) throw new Error("Http Error");
+            const data = await response.json();
+            console.log(data);
+        }catch(err){
+            console.log(`Error fetching data ${err}`)
+        }     
+    }
 
-
+     fetchQuestion();
 
     return(
         <>
