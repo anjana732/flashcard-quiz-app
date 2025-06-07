2161763fea015d8d331be0bbbf4e28c174117595
anjana732 <anjanasingh1257@gmail.com>
2025-06-07 11:38:29 +0530

    add openai services

---
+++ backend/src/services/openai.service.js
@@ -0,0 +1,17 @@
+import { openai } from "../config/openai.config";
+
+const  gethint = async (question)=> {
+    const response = await openai.chat.completions.create({
+        model: "gpt-4",
+        messages: [
+            {role: "system", content: "You are a helpful assistant" },
+            {role: "user", content:`Give a hint for ${question}`}
+        ],
+        temperature: 0.5,
+        max_tokens: 60
+    })
+
+    return response.choices[0].message.content?.trim() || "No hint available" ;
+}
+
+export default gethint;
