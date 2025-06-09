8d18bec57bd5f43a175107a1641685aed03cc556
anjana732 <anjanasingh1257@gmail.com>
2025-06-09 16:16:51 +0530

    Add websocket configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,23 +2,45 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import hintRoute from './routes/openai.route.js'
-
+import {Server} from 'socket.io';
+import http from 'http';
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors:{
+    origin : "*"
+  }
+})
 
-app.use(cors());
-app.use(express.json());
+io.on("connection", (socket) => {
+  console.log("Frontend connected: ", socket.id);
+});
 
 app.post('/github-event', (req, res) => {
   const payload = req.body;
   res.status(200).send("Received");
   console.log(payload);
+
+io.emit("github_update", {
+    message: "New update pushed to GitHub!",
+    repo: payload.repository.full_name,
+    commits: payload.commits,
+  });
+
 });
+
+
+
+
+ 
 
 app.use('/api', hintRoute);
 
